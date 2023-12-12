fddd2da3d3279cec2d350f309a6a6cc3f427a31b
jeremy <jeremy.guillaume@centralesupelec.fr>
2023-12-12 14:36:48 +0100

    modification du filtre

--- src/sobel.c
+++ src/sobel.c
@@ -1,14 +1,13 @@
 /*
-        ============================================================================
-        Name        : sobel.c
-        Author      : kdesnos
-        Version     : 1.2
-        Copyright   : CECILL-C
-        Description :
-        ============================================================================
+ ============================================================================
+ Name        : average_filter.c
+ Author      : [Your Name]
+ Version     : 1.0
+ Copyright   : [Your Copyright]
+ Description : Averaging filter implementation
+ ============================================================================
 */
 
-#include <math.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -20,15 +19,12 @@ void sobel(int width, int height, unsigned char *input, unsigned char *output) {
   // Apply the filter
   for (j = 1; j < height - 1; j++) {
     for (i = 1; i < width - 1; i++) {
-      int gx = -input[(j - 1) * width + i + 1] - 2 * input[j * width + i - 1] -
-               input[(j + 1) * width + i - 1] + input[(j - 1) * width + i + 1] +
-               2 * input[j * width + i + 1] + input[(j + 1) * width + i + 1];
-      int gy = -input[(j - 1) * width + i - 1] -
-               2 * input[(j - 1) * width + i] - input[(j - 1) * width + i + 1] +
-               input[(j + 1) * width + i - 1] + 2 * input[(j + 1) * width + i] +
-               input[(j + 1) * width + i + 1];
+      // Compute the average of the neighboring pixels
+      int sum = input[(j - 1) * width + i - 1] + input[(j - 1) * width + i] + input[(j - 1) * width + i + 1] +
+                input[j * width + i - 1] + input[j * width + i] + input[j * width + i + 1] +
+                input[(j + 1) * width + i - 1] + input[(j + 1) * width + i] + input[(j + 1) * width + i + 1];
 
-      output[(j-1)*width + i] = fminf(sqrtf(gx * gx + gy * gy), 255);
+      output[(j - 1) * width + i] = sum / 9; // Divide by the number of neighbors for average
     }
   }
 
@@ -38,3 +34,4 @@ void sobel(int width, int height, unsigned char *input, unsigned char *output) {
     output[(j + 1) * width - 1] = 0;
   }
 }
+
